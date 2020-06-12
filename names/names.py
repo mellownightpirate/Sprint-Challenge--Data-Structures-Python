cdbae2cda7b1db21a14832131e6c7eeb9266336a
mellownightpirate <a@hasan.co>
2020-06-12 08:58:13 +0100

    Find duplicate names

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree
 
 start_time = time.time()
 
@@ -10,13 +11,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+print(names_1[0])
 
+duplicates = []  # Return the list of duplicates in this data structure
+tree = BinarySearchTree(names_1[0])
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+for name in names_1:
+    tree.insert(name)
+
+for name in names_2:
+    if tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
